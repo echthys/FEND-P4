baa0bb453d48a9db01ec4b4de283e8f3b8aa9350
Ethan <81968564+echthys@users.noreply.github.com>
2021-07-28 20:21:46 +0100

    feat: added sass loaders and fixed module versions

--- src/client/index.js
+++ src/client/index.js
@@ -2,3 +2,8 @@ import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
 
+import './styles/header.scss'
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
